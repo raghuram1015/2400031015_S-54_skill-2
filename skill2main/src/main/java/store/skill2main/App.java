bac58061c13bfb07ade60d8920d1586ee37313a7
Your Name <raghuramp456@gmail.com>
2026-01-02 07:50:26 +0530

    First Commit

---
+++ skill2main/src/main/java/store/skill2main/App.java
@@ -0,0 +1,13 @@
+package store.skill2main;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
